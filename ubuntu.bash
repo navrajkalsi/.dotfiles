4fae8fb6a51c26924405020aed47f44b3ac2d0d1
Navraj Kalsi <navrajkalsi@icloud.com>
2025-09-17 07:39:42 -0400

    ubuntu.bash

--- ubuntu.bash
+++ ubuntu.bash
@@ -4,18 +4,18 @@
 ORG_DIR=$(pwd);
 
 # Package manager stuff
-echo "\nInstalling Packages\n"
+printf "\nInstalling Packages\n"
 sudo apt update
 sudo apt install build-essential cmake stow -y
 
 # .dotfiles
-echo "\nMoving .bashrc\n"
+printf "\nMoving .bashrc\n"
 mv "$HOME/.bashrc "$HOME/.bashrc-org
-echo "\nStowing configs\n"
+printf "\nStowing configs\n"
 stow bash nvim tmux fzf dircolors git
 
 # Neovim
-echo "\nInstalling Neovim\n"
+printf "\nInstalling Neovim\n"
 git clone https://github.com/neovim/neovim -b stable --depth 1 "$HOME/navraj/neovim"
 cd "$HOME/navraj/neovim"
 make CMAKE_BUILD_TYPE=RelWithDebInfo
@@ -23,31 +23,29 @@ sudo make install
 cd "$ORG_DIR"
 
 # Neovim plugin manager
-echo "\nInstalling Neovim Plugin Manager\n"
+printf "\nInstalling Neovim Plugin Manager\n"
 git clone https://github.com/wbthomason/packer.nvim "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim" --depth 1
-nvim --headless -c 'autocmd User PackerCompelete quitall' -c 'PackerSync'
 
 # Fzf
-echo "\nInstalling Fzf\n"
+printf "\nInstalling Fzf\n"
 git clone https://github.com/junegunn/fzf.git "$HOME/navraj/fzf" --depth 1
 "$HOME/navraj/fzf/install"
 
 # Zoxide
-echo "\nInstalling Zoxide\n"
+printf "\nInstalling Zoxide\n"
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sudo sh
 
-echo "\nSourcing new bashrc\n"
+printf "\nSourcing new bashrc\n"
 source "$HOME/.bashrc"
 
-remove_dir="y"
 read -p "Remove $HOME/navraj dir (Y/n): " remove_dir
-remove_dir=${remove_dir..}
+remove_dir=${remove_dir,,}
 
 if [[ "$remove_dir" == "n" || "$remove_dir" == "no" ]]; then
-    echo "\nSkipped removing $HOME/navraj\n"
+    printf "\nSkipped removing $HOME/navraj\n"
 else
-    echo "\nRemoving $HOME/navraj\n"
-    rm -rf $HOME/navraj
+    printf "\nRemoving $HOME/navraj\n"
+    rm -rf "$HOME/navraj"
 fi
 
-echo "\nDone."
+printf "\nDone."
