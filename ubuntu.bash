8c9eb4ac50398c694b044b0f1fe5d87f9faefbcd
Navraj Kalsi <navrajkalsi@icloud.com>
2025-09-17 06:50:20 -0400

    ubuntu.bash

--- ubuntu.bash
+++ ubuntu.bash
@@ -1,30 +1,53 @@
 #!/bin/bash
 
+# Run after cloning .dotfiles, from .dotfiles
+ORG_DIR=$(pwd);
+
 # Package manager stuff
+echo "\nInstalling Packages\n"
 sudo apt update
-sudo apt install build-essential cmake stow
+sudo apt install build-essential cmake stow -y
 
 # .dotfiles
-mv ~/.bashrc ~/.bashrc-org
+echo "\nMoving .bashrc\n"
+mv "$HOME/.bashrc "$HOME/.bashrc-org
+echo "\nStowing configs\n"
 stow bash nvim tmux fzf dircolors git
 
 # Neovim
-git clone https://github.com/neovim/neovim -b stable --depth 1 ~/navraj/neovim
-cd ~/navraj/neovim
+echo "\nInstalling Neovim\n"
+git clone https://github.com/neovim/neovim -b stable --depth 1 "$HOME/navraj/neovim"
+cd "$HOME/navraj/neovim"
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
-cd -
+cd "$ORG_DIR"
 
 # Neovim plugin manager
-git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim --depth 1
+echo "\nInstalling Neovim Plugin Manager\n"
+git clone https://github.com/wbthomason/packer.nvim "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim" --depth 1
 nvim --headless -c 'autocmd User PackerCompelete quitall' -c 'PackerSync'
 
 # Fzf
-git clone https://github.com/junegunn/fzf.git ~/navraj/fzf --depth 1
-~/navraj/fzf/install
+echo "\nInstalling Fzf\n"
+git clone https://github.com/junegunn/fzf.git "$HOME/navraj/fzf" --depth 1
+"$HOME/navraj/fzf/install"
 
 # Zoxide
+echo "\nInstalling Zoxide\n"
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sudo sh
 
-source ~/.bashrc
-rm -rf ~/navraj
+echo "\nSourcing new bashrc\n"
+source "$HOME/.bashrc"
+
+remove_dir="y"
+read -p "Remove $HOME/navraj dir (Y/n): " remove_dir
+remove_dir=${remove_dir..}
+
+if [[ "$remove_dir" == "n" || "$remove_dir" == "no" ]]; then
+    echo "\nSkipped removing $HOME/navraj\n"
+else
+    echo "\nRemoving $HOME/navraj\n"
+    rm -rf $HOME/navraj
+fi
+
+echo "\nDone."
